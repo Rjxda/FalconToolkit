debf89bc03398bf1e102ef862ebf529c19a9f2db
ACharLuk <acharluk@gmail.com>
2014-12-21 14:42:13 +0100

    Added reboot options

---
+++ src/com/acharluk/falcontoolkit/Command.java
@@ -0,0 +1,49 @@
+package com.acharluk.falcontoolkit;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by ACharLuk on 21/12/2014.
+ */
+public class Command {
+
+    static String adb = "adb.exe";
+    static String fastboot = "fastboot.exe";
+
+    public static String rebootToFastboot() {
+        send(adb, "reboot bootloader");
+        return "Rebooted into fastboot";
+    }
+
+    public static String rebootToRecovery() {
+        send(adb, "reboot recovery");
+        return "Rebooted into recovery";
+    }
+
+    public static String rebootToNormalFromADB() {
+        send(adb, "reboot");
+        return "Rebooted into android";
+    }
+
+    public static String rebootToNormalFromFastboot() {
+        send(fastboot, "reboot");
+        return "Rebooted into android";
+    }
+
+    private static String send(String mode, String arguments) {
+        String output = "";
+        try {
+            Process process = Runtime.getRuntime().exec("cmd /c res\\" + mode + " " + arguments);
+            InputStream in = process.getInputStream();
+            int ch;
+            while((ch = in.read()) != -1) {
+                output+=((char)ch);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output;
+    }
+
+}
--- src/com/acharluk/falcontoolkit/Main.java
+++ src/com/acharluk/falcontoolkit/Main.java
@@ -3,6 +3,8 @@ package com.acharluk.falcontoolkit;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Command.rebootToFastboot();
     }
+
 }
